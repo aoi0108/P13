e20df4395892c3e40587bcdfac014a66a97bd4aa
Karin Umebayashi <g2120507@is.ocha.ac.jp>
2023-11-01 16:35:54 +0900

    画像拡大表示実装

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function showFood() {
     //結果反映用のエレメントを取得
     let result = document.getElementById("result");
     let name = document.getElementById("foodName");
+    let link = document.getElementById("foodLink");
     let img = document.getElementById("foodImg");
 
     //選択肢取得用のエレメントを取得
@@ -37,21 +38,29 @@ function showFood() {
         if (checkedFoodType == "japaneseFood") {
             //外食・和食
             name.innerHTML = "お寿司";
+            link.setAttribute("href", "sushi.jpg");
+            link.setAttribute("data-title","お寿司");
             img.setAttribute("src", "sushi.jpg");
             title = "お寿司";
         } else if (checkedFoodType == "chineseFood") {
             //外食・中華
             name.innerHTML = "小籠包";
+            link.setAttribute("href", "xiaolongbao.jpg");
+            link.setAttribute("data-title","小籠包");
             img.setAttribute("src", "xiaolongbao.jpg");
             title ="小籠包";
         } else if (checkedFoodType == "westernFood") {
             //外食・洋食
             name.innerHTML = "ピザ";
+            link.setAttribute("href", "pizza.jpg");
+            link.setAttribute("data-title","ピザ");
             img.setAttribute("src", "pizza.jpg");
             title ="ピザ";
         } else if (checkedFoodType == "ethnicFood") {
             //外食・エスニック
             name.innerHTML = "カレー";
+            link.setAttribute("href", "curry.jpg");
+            link.setAttribute("data-title","カレー");
             img.setAttribute("src", "curry.jpg");
             title ="カレー";
         }
@@ -60,21 +69,29 @@ function showFood() {
         if (checkedFoodType == "japaneseFood") {
             //テイクアウト・和食
             name.innerHTML = "牛めし";
+            link.setAttribute("href", "gyu-don.jpg");
+            link.setAttribute("data-title","牛めし");
             img.setAttribute("src", "gyu-don.jpg");
             title ="牛めし";
         } else if (checkedFoodType == "chineseFood") {
             //テイクアウト・中華
             name.innerHTML = "餃子";
+            link.setAttribute("href", "jiaozi.jpg");
+            link.setAttribute("data-title","餃子");
             img.setAttribute("src", "jiaozi.jpg");
             title ="餃子";
         } else if (checkedFoodType == "westernFood") {
             //テイクアウト・洋食
             name.innerHTML = "ハンバーガー";
+            link.setAttribute("href", "hamburger.jpg");
+            link.setAttribute("data-title","ハンバーガー");
             img.setAttribute("src", "hamburger.jpg");
             title ="ハンバーガー";
         } else if (checkedFoodType == "ethnicFood") {
             //テイクアウト・エスニック
             name.innerHTML = "トムヤムクン";
+            link.setAttribute("href", "tomyamkun.jpg");
+            link.setAttribute("data-title","トムヤムクン");
             img.setAttribute("src", "tomyamkun.jpg");
             title ="トムヤムクン";
         }
@@ -83,21 +100,29 @@ function showFood() {
         if (checkedFoodType == "japaneseFood") {
             //自炊・和食
             name.innerHTML = "さばの味噌煮";
+            link.setAttribute("href", "sabanomisoni.jpg");
+            link.setAttribute("data-title","さばの味噌煮");
             img.setAttribute("src", "sabanomisoni.jpg");
             title ="さばの味噌煮";
         } else if (checkedFoodType == "chineseFood") {
             //自炊・中華
             name.innerHTML = "棒棒鶏";
+            link.setAttribute("href", "bangbangji.jpg");
+            link.setAttribute("data-title","棒棒鶏");
             img.setAttribute("src", "bangbangji.jpg");
             title ="棒棒鶏";
         } else if (checkedFoodType == "westernFood") {
             //自炊・洋食
             name.innerHTML = "ビーフシチュー";
+            link.setAttribute("href", "beafStew.jpg");
+            link.setAttribute("data-title","ビーフシチュー");
             img.setAttribute("src", "beafStew.jpg");
             title ="ビーフシチュー";
         } else if (checkedFoodType == "ethnicFood") {
             //自炊・エスニック
             name.innerHTML = "生春巻き";
+            link.setAttribute("href", "springRoll.jpg");
+            link.setAttribute("data-title","生春巻き");
             img.setAttribute("src", "springRoll.jpg");
             title ="生春巻き";
         }
